a40bc123db1ad3a7a78cd2aba5a388da153ecb22
Herlyn Mae <Mitchellllllll>
2025-01-23 15:00:11 +0800

    Contact 100%

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ burgir.addEventListener("click", () => {
     headerNav.classList.toggle("open");
 });
 
+const b2Top = document.querySelector(".backToTop");
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 300) {
+    b2Top.classList.add("open");
+  } else {
+    b2Top.classList.remove("open");
+  }
+});
